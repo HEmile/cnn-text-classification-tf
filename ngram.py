2192e016e6190a83a401e0c925a6947a1300e87a
Rezka Aufar Leonandya <rezka.aufar@gmail.com>
2017-06-16 16:06:03 +0200

    fixed error in sampling ngram

--- ngram.py
+++ ngram.py
@@ -11,6 +11,7 @@ def get_ngram(n, data):
     matrix_prob = matrix_freq / float(np.sum(matrix_freq))
     return matrix_terms, matrix_prob, matrix_freq
 
+
 def sample_ngram(num_sample, X):
   # sample unigram
   unigram_terms, unigram_prob, _ = get_ngram(1, X)
@@ -18,19 +19,28 @@ def sample_ngram(num_sample, X):
   # sample bigram
   bigram_terms, bigram_prob, _ = get_ngram(2, X)
   bigram_sample = []
+  # sample trigram
+  trigram_terms, trigram_prob, _ = get_ngram(3, X)
+  trigram_sample = []
   for i in range(num_sample):
+    # sampling bigram
     b_index = [j for j in range(len(bigram_terms)) if unigram_sample[i] == bigram_terms[j].split()[0]]
     bg_prob = bigram_prob[b_index] / np.sum(bigram_prob[b_index])
     bg_terms = bigram_terms[b_index]
     bg_sample = np.random.choice(bg_terms, 1, p=bg_prob)
     bigram_sample.append(bg_sample[0])
-  # sample trigram
-  trigram_terms, trigram_prob, _ = get_ngram(3, X)
-  trigram_sample = []
-  for i in range(num_sample):
-    t_index = [j for j in range(len(trigram_terms)) if
-               bigram_sample[i].split()[0] == trigram_terms[j].split()[0] and
-               bigram_sample[i].split()[1] == trigram_terms[j].split()[1]]
+
+    t_index = []
+    # resample if no trigram exists given a bigram
+    while len(t_index) == 0:
+      t_index = [j for j in range(len(trigram_terms)) if
+                 bigram_sample[i].split()[0] == trigram_terms[j].split()[0] and
+                 bigram_sample[i].split()[1] == trigram_terms[j].split()[1]]
+      b_index = [j for j in range(len(bigram_terms)) if unigram_sample[i] == bigram_terms[j].split()[0]]
+      bg_prob = bigram_prob[b_index] / np.sum(bigram_prob[b_index])
+      bg_terms = bigram_terms[b_index]
+      bg_sample = np.random.choice(bg_terms, 1, p=bg_prob)
+      bigram_sample[i] = bg_sample[0]
     tg_prob = trigram_prob[t_index] / np.sum(trigram_prob[t_index])
     tg_terms = trigram_terms[t_index]
     tg_sample = np.random.choice(tg_terms, 1, p=tg_prob)
