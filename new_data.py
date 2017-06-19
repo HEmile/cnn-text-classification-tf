e612d0a0f23d5d75df10a01cac31aa371726a3a1
RoderickvanderWeerdt <rvanderweerdt@hotmail.com>
2017-06-19 16:18:14 +0200

    changed new_data to support multiple samples per word, and using unigram instead of ngram

--- new_data.py
+++ new_data.py
@@ -1,9 +1,9 @@
 from numpy import floor, ceil
-from ngram import sample_ngram
+from unigram import sample_ngram
 
 def words_from_ngram_distribution(distribution, sample_count=1):
     if distribution == "unigram" or distribution == "bigram" or distribution == "trigram":
-        ug, bg, tg = sample_ngram(sample_count)
+        ug = sample_ngram(sample_count)
         if distribution == "unigram":
             return ug
         if distribution == "bigram":
@@ -11,45 +11,49 @@ def words_from_ngram_distribution(distribution, sample_count=1):
         if distribution == "trigram":
             return tg
     else:
+        print("The ngram implementation uses either unigram, bigram or trigram")
         return(["PLANETS", "PLANETS", "PLANETS"])
 
-def count_samples(sentence, window_size):
+def count_samples(sentence, S, window_size):
     sample_count = 0
     for index in range(len(sentence)):
         if ((index - int(floor(window_size/2))) >= 0) and ((index + int(ceil(window_size/2))) <= len(sentence)):
             sample_count += 1
-    return sample_count
+    return sample_count * S
 
-def get_ngram_mutations(sentence, distribution):
+def get_S_ngram_mutations(sentence, S, distribution):
     if distribution == "unigram": window_size = 1
     if distribution == "bigram": window_size = 2
     if distribution == "trigram": window_size = 3
 
+
     sentence = sentence.split()
 
-    n_samples = count_samples(sentence, window_size)
+    n_samples = count_samples(sentence, S, window_size)
     mutated_words = words_from_ngram_distribution(distribution, n_samples)
 
     all_mutated_words = list(mutated_words)
 
     mutations = []
     for index, word in enumerate(sentence):
-        if ((index - int(floor(window_size/2))) >= 0) and ((index + int(ceil(window_size/2))) <= len(sentence)):
-            mutation = sentence[:]
-            indices = range(index - int(floor(window_size/2)), (index + int(ceil(window_size/2))))
+        for _ in range(S):
+            if ((index - int(floor(window_size/2))) >= 0) and ((index + int(ceil(window_size/2))) <= len(sentence)):
+                mutation = sentence[:]
+                indices = range(index - int(floor(window_size/2)), (index + int(ceil(window_size/2))))
 
-            mutated_words = all_mutated_words.pop()
-            for mutated_index, mutated_word in zip(indices, mutated_words.split()):
-                mutation[mutated_index] = mutated_word
+                mutated_words = all_mutated_words.pop()
+                for mutated_index, mutated_word in zip(indices, mutated_words.split()):
+                    mutation[mutated_index] = mutated_word
 
-            mutations.append(' '.join(mutation))
+                mutations.append(' '.join(mutation))
     return mutations
 
-example = False
+example = True
 #example of how to use the function
 if example:
     sentence = "a disturbing and frighteningly evocative assembly of imagery and hypnotic music composed by philip glass . "
-    distribution = "trigram"
-    mutations = get_ngram_mutations(sentence, distribution)
+    distribution = "unigram"
+    mutations = get_S_ngram_mutations(sentence, 3, distribution)
+    print("mutating:", sentence, '\n')
     for mutation in mutations:
         print(mutation)
---
+++ unigram.py
@@ -0,0 +1,20 @@
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+import data_helpers as dh
+X, y = dh.load_data_and_labels("data/rt-polaritydata/rt-polarity.pos",
+                                    "data/rt-polaritydata/rt-polarity.neg")
+def get_ngram(n, data):
+    vectorizer = CountVectorizer(ngram_range=(n,n), min_df=0, token_pattern=r"\b\w+\b") # retain uninformative words
+    X = vectorizer.fit_transform(data)
+    matrix_terms = np.array(vectorizer.get_feature_names())
+    matrix_freq = np.asarray(X.sum(axis=0)).ravel()
+    matrix_prob = matrix_freq / float(np.sum(matrix_freq))
+    return matrix_terms, matrix_prob, matrix_freq
+
+
+def sample_ngram(num_sample):#, X):
+  # sample unigram
+  unigram_terms, unigram_prob, _ = get_ngram(1, X)
+  unigram_sample = np.random.choice(unigram_terms, num_sample, p=unigram_prob)
+  
+  return unigram_sample
