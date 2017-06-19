5642591492ed5e6bb972a2792eff858abb316ba6
Rezka Aufar Leonandya <rezka.aufar@gmail.com>
2017-06-19 15:12:06 +0200

    ngram retain uninformative word

--- ngram.py
+++ ngram.py
@@ -4,7 +4,7 @@ import data_helpers as dh
 X, y = dh.load_data_and_labels("data/rt-polaritydata/rt-polarity.pos",
                                     "data/rt-polaritydata/rt-polarity.neg")
 def get_ngram(n, data):
-    vectorizer = CountVectorizer(ngram_range=(n,n))
+    vectorizer = CountVectorizer(ngram_range=(n,n), min_df=0, token_pattern=r"\b\w+\b") # retain uninformative words
     X = vectorizer.fit_transform(data)
     matrix_terms = np.array(vectorizer.get_feature_names())
     matrix_freq = np.asarray(X.sum(axis=0)).ravel()
