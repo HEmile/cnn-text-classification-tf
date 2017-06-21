4bc7f147d77a3dd08f10cdc236488d96294bc75b
RoderickvanderWeerdt <rvanderweerdt@hotmail.com>
2017-06-21 15:13:30 +0200

    updated trigrams distribution to use dicts as bigrams do, cleaned up new_continuous_data

--- continuous_distribution.py
+++ continuous_distribution.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 import data_helpers as dh
-from unigram import sample_unigram
+# from unigram import sample_unigram
 
 class nGrams:
     def __init__(self, X=""):
@@ -9,6 +9,7 @@ class nGrams:
             X, y = dh.load_data_and_labels("data/rt-polaritydata/rt-polarity.pos",
                                            "data/rt-polaritydata/rt-polarity.neg")
         self.unigram_terms, self.unigram_prob, _ = get_ngram(1, X)
+
         self.bigram_terms, self.bigram_prob, _ = get_ngram(2, X)
         self.bigram_dict = {}
         for i in range(len(self.bigram_terms)):
@@ -19,7 +20,24 @@ class nGrams:
             else:
                 self.bigram_dict[terms[0]] = ([terms[1]], [self.bigram_prob[i]])
         # self.bigram_dict = get_ngramDict
+
         self.trigram_terms, self.trigram_prob, _ = get_ngram(3, X)
+        self.trigram_dict = {}
+        for i in range(len(self.trigram_terms)):
+            terms = self.trigram_terms[i].split()
+            if terms[0] in self.trigram_dict:
+                if terms[1] in self.trigram_dict[terms[0]]:
+                    self.trigram_dict[terms[0]][terms[1]][0].append(terms[2])
+                    self.trigram_dict[terms[0]][terms[1]][1].append(self.trigram_prob[i])
+                else:
+                    self.trigram_dict[terms[0]][terms[1]] = ([terms[2]], [self.trigram_prob[i]])
+            else:
+                self.trigram_dict[terms[0]] = {}
+                self.trigram_dict[terms[0]][terms[1]] = ([terms[2]], [self.trigram_prob[i]])
+
+    def sample_unigram(self, num_sample=1):
+        return np.random.choice(self.unigram_terms, num_sample, p=self.unigram_prob)
+
 
 
 def get_ngram(n, data):
@@ -31,60 +49,47 @@ def get_ngram(n, data):
     return matrix_terms, matrix_prob, matrix_freq
 
 
-# works with either one or two words (resulting in bigrams or trigrams)
+# works with either two or three words (resulting in bigrams or trigrams)
 def sample_continous(words, ngrams):
-    # X, y = dh.load_data_and_labels("data/rt-polaritydata/rt-polarity.pos",
-    # "data/rt-polaritydata/rt-polarity.neg")
 
-    # sample trigram
-    if len(words) >= 3:
-        trigram_terms = ngrams.trigram_terms
-        trigram_prob = ngrams.trigram_prob
+    # sampling bigram
+    if len(words) == 2:
+        if not words[0] in ngrams.bigram_dict:
+            return [words[0], ngrams.sample_unigram()[0]]
+        bigrams, bigram_prob = ngrams.bigram_dict[words[0]]
+        if words[1] in bigrams:
+            windex = bigrams.index(words[1])
+            bigrams = list(bigrams)
+            del bigrams[windex]
+            bigram_prob = list(bigram_prob)
+            del bigram_prob[windex]
 
-    # print("words:", words)
-    t_index = []
-    # resample if no trigram exists given a bigram
-    while len(t_index) == 0:
-        # sampling bigram
-        if len(words) == 2:
-            if not words[0] in ngrams.bigram_dict:
-                return [words[0], sample_unigram(1)[0]]
-            bigrams, bigram_prob = ngrams.bigram_dict[words[0]]
-            if words[1] in bigrams:
-                windex = bigrams.index(words[1])
-                bigrams = list(bigrams)
-                del bigrams[windex]
-                bigram_prob = list(bigram_prob)
-                del bigram_prob[windex]
-
-            if len(bigrams) < 1:
-                return [words[0], sample_unigram(1)[0]]
-            else:
-                bg_prob = bigram_prob / np.sum(bigram_prob)
-                bg_sample = np.random.choice(bigrams, 1, p=bg_prob)[0]
-                return [words[0], bg_sample]  # uncomment this if a trigram should be based on only one word
+        if len(bigrams) < 1:
+            return [words[0], ngrams.sample_unigram()[0]] #unigram sampling if no word was predicted
         else:
-            bigram_sample = words
+            bg_prob = bigram_prob / np.sum(bigram_prob)
+            bg_sample = np.random.choice(bigrams, 1, p=bg_prob)[0]
+            return [words[0], bg_sample]  # uncomment this if a trigram should be based on only one word
 
-        if len(bigram_sample) > 0:
-            t_index = [j for j in range(len(trigram_terms)) if
-                       bigram_sample[0] == trigram_terms[j].split()[0] and
-                       bigram_sample[1] == trigram_terms[j].split()[1]]
-        else:
-            print("WARNING, no trigrams found because no bigrams were found!")
-            break
+    # sampling trigrams
+    if len(words) == 3:
+        if not words[0] in ngrams.trigram_dict:
+            return [words[0], words[1], ngrams.sample_unigram()[0]] #unigram sampling if no word was predicted
+        if not words[1] in ngrams.trigram_dict[words[0]]:
+            return [words[0], words[1], ngrams.sample_unigram()[0]] #unigram sampling if no word was predicted
+        trigrams, trigram_prob = ngrams.trigram_dict[words[0]][words[1]]
+        if words[2] in trigrams:
+            windex = trigrams.index(words[2])
+            trigrams = list(trigrams)
+            del trigrams[windex]
+            trigram_prob = list(trigram_prob)
+            del trigram_prob[windex]
 
-        n_indices = len(t_index)
-        if n_indices > 0:
-            tg_sample = words
-            while n_indices > 0 and (tg_sample[2] == words[2]):
-                tg_prob = trigram_prob[t_index] / np.sum(trigram_prob[t_index])
-                tg_terms = trigram_terms[t_index]
-                tg_sample = np.random.choice(tg_terms, 1, p=tg_prob)
-                tg_sample = tg_sample[0].split()
-                print("loop")
-                n_indices = n_indices - 1
+        if len(trigrams) < 1:
+            return [words[0], words[1], ngrams.sample_unigram()[0]] #unigram sampling if no word was predicted (other then the original word)
         else:
-            # print("WARNING, no trigrams found because no bigrams were found!")
-            return ("XXX")
-    return tg_sample
+            tg_prob = trigram_prob / np.sum(trigram_prob)
+            tg_sample = np.random.choice(trigrams, 1, p=tg_prob)[0]
+            return [words[0], words[1], tg_sample]
+
+    return "OEI"
--- new_continuous_data.py
+++ new_continuous_data.py
@@ -5,80 +5,54 @@ from new_data import words_from_ngram_distribution
 
 #dummy placefiller function
 def word_from_distribution(ngrams, words):
-    # ug, bg, tg = sample_ngram(1)
-    # if distribution == "unigram":
-    if len(words) == 1:
+    if len(words) == 1: #"unigram":
         return unigram
-    # if distribution == "bigram":
-    if len(words) == 2:
+    if len(words) == 2: # "bigram":
         return sample_continous(words, ngrams)
-    # if distribution == "trigram":
-    if len(words) == 3:
+    if len(words) == 3: # "trigram":
         return sample_continous(words, ngrams)
     else:
         return(["PLANETS", "PLANETS", "PLANETS"])
 
-#only works with uneven window_size (as in, symmetrical around one index)
 def get_mutations(sentence, S, window_size=2, window_mode="preceding"):
     ngrams = nGrams()
     sentence = sentence.split()
 
-    # print("mutating sentence: ", sentence, '\n')
-
     mutations = []
     for index in range(len(sentence)+1):
-        # mutation = sentence[:]
         indices = []
         words = []
         mutated_words = []
-        # if window_mode == "symmetric" and ((index - int(floor(window_size/2))) >= 0) and ((index + int(ceil(window_size/2))) <= len(sentence)): #only symmetric
-        #     indices = range(index - int(floor(window_size/2)), (index + int(ceil(window_size/2))))
-        #     words = mutation[(index - int(floor(window_size/2))):(index + int(ceil(window_size/2)))]
-        #     mutated_words = word_from_distribution(ngrams, words) #SKIPGRAM (not really, but what we call a skipgram)
-        # print("index:", index)
-        # print("index-window_size:", index-window_size)
         if window_mode == "preceding" and (index - window_size >= 0):
             for _ in range(S):
                 mutation = sentence[:]
                 indices = range(index - window_size, index)
                 words = mutation[index - window_size:index]
                 mutated_words = word_from_distribution(ngrams, words)
-            # if window_mode == "following" and (index + window_size <= len(sentence)):
-            #     indices = range(index, index + window_size)
-            #     words = mutation[index:index + window_size]
-            #     mutated_words = word_from_distribution(ngrams, words) #weet nog niet hoe!
-
-            # print("original words", words)
-            # print("mutated  words", mutated_words)
 
                 for mutated_index, mutated_word in zip(indices, mutated_words):
                     mutation[mutated_index] = mutated_word
 
-            # print(indices)
-                # print(' '.join(mutation))
                 mutations.append(' '.join(mutation))
-        elif window_mode == "preceding" and (index - window_size == -1):
-            #first word unigram for bigram
+        elif window_mode == "preceding" and (index - window_size >= -(window_size-1)):
+        #only the words which can not be predicted by ngram (example: bigram -> index 0 or trigram -> index 0, 1)
             mutated_words = words_from_ngram_distribution("unigram", S)
 
             all_mutated_words = list(mutated_words)
             for _ in range(S):
                 mutation = sentence[:]
-
                 mutated_words = all_mutated_words.pop()
-                for mutated_index, mutated_word in zip(range(0,1), mutated_words.split()):
+                for mutated_index, mutated_word in zip(range(index-1,index), mutated_words.split()):
                     mutation[mutated_index] = mutated_word
-
-                mutations.append(' '.join(mutation) + "***")
-
+                mutations.append(' '.join(mutation))
     return mutations
 
 
-example = False
+example = True
 if example:
     sentence = "a disturbing and frighteningly evocative assembly of imagery and hypnotic music composed by philip glass . "
-    S = 2
-    window_size = 2
+    S = 20
+    window_size = 3
     mutations = get_mutations(sentence, S, window_size)
 
     print(sentence+'\n')
@@ -90,4 +64,3 @@ if example:
         if i == 0:
             i = S
             print()
-
